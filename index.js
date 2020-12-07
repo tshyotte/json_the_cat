b3ecd5df319ab9122291841b5ba9b1b4be83b814
Odette Bahome <odettebahome@gmail.com>
2020-12-07 11:54:40 +0000

    edited index file

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 const {fetchDescription} = require('./breedFetcher');
 
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+let searchBreed = process.argv.slice(2)[1];
+let searchUrl = `${process.argv.slice(2)[0]}?q=${searchBreed}`;
+
+fetchBreedDescription(searchBreed, searchUrl);
+rl.close();
