cf6e74af1bdb01f71b95553cfc9e79b436112356
Odette Bahome <odettebahome@gmail.com>
2020-12-07 00:03:26 +0000

    edited file

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,23 +4,30 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+
 let searchBreed = process.argv.slice(2)[1];
 let searchUrl = `${process.argv.slice(2)[0]}?q=${searchBreed}`;
 
-https.get(searchUrl, (resp) => {
-  let data = '';
-
-  // A chunk of data has been recieved.
-  resp.on('data', (body) => {
-    data += body;
+//const fetchBreedDescription = function(breedName, callback) {
+  https.get(searchUrl, (resp) => {
+    let data = '';
+  
+    // A chunk of data has been recieved.
+    resp.on('data', (body) => {
+      data += body;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(JSON.parse(data));
+    });
+  
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
   });
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+//};
 rl.close();
+
+//module.exports = { fetchBreedDescription };
